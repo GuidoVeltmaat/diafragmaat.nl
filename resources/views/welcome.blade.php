4bec5da62ce5a29e1031020b36da733875da2c80
Guido Veltmaat <info@diafragmaat.com>
2024-10-09 13:24:17 +0200

    build ignored > un ignored

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Diafragmaat.nl</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Styles -->
         @vite('resources/css/app.css')
         @fluxStyles
@@ -17,24 +14,18 @@
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
     <flux:main>
     <div class="flex items-center justify-center h-screen">
-        <div class="p-8 rounded-lg shadow-md space-y-8">
+        <div class="p-8 rounded-lg bg-gray-500/20 space-y-8">
             <flux:heading level="1" size="lg">Hello Fluxer!</flux:heading>
             <flux:subheading>
-                <flux:modal.trigger name="delete-profile">
+                <flux:modal.trigger name="opendarnit">
                     <flux:button>open modal</flux:button>
                 </flux:modal.trigger>
-
-                <flux:modal name="delete-profile" class="min-w-[22rem] space-y-6">
+                <flux:modal name="opendarnit" class="min-w-[22rem] space-y-6">
                     <div>
-
-                    <flux:heading size="lg">Delete project?</flux:heading>
+                    <flux:heading size="lg">NOW you see me?</flux:heading>
                     </div>
                 </flux:modal>
-
             </flux:subheading>
-{{--            <flux:separator ></flux:separator>--}}
-
-
             <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                 Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
             </footer>
